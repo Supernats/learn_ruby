775154ec8c7a3b56ebdb35a3688a33448a1c762e
Supernats <nathanseither@gmail.com>
2013-10-11 01:27:26 -0700

    Finished through 02

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,40 @@
+def add (num1, num2)
+	num1 + num2
+end
+
+def subtract(num1, num2)
+	num1 - num2
+end
+
+def sum(nums)
+	out = 0
+	nums.each do |num|
+		out += num
+	end
+	out
+end
+
+def multiply(nums)
+	out = 1
+	nums.each do |num|
+		out *= num
+	end
+	out
+end
+
+def power(base, power)
+	out = 1
+	power.times do
+		out *= base
+	end
+	out
+end
+
+def factorial(num)
+	out = 1
+	nums = Array (1..num)
+	nums.each do |i|
+		out *= i
+	end
+	out
+end
