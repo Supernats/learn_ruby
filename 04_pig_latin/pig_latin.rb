5d8a1ec57b45a341bc21adc777a1bf5bb201aa56
Supernats <nathanseither@gmail.com>
2013-10-14 22:47:56 -0700

    Write pig latin

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,30 @@
+VOWELS = ["a", "e", "i", "o", "u"]
+
+
+def translate(message)
+	arr = message.split
+	out = ""
+	arr.each_with_index do |word, idx|
+		out += "#{translate_word(word)}"
+		out += " " unless idx == arr.count - 1
+	end
+	out
+end
+
+
+def translate_word(word)
+	arr = word.split("")
+	swap = ""
+	arr.each_with_index do |letter, idx|
+		if letter == "u" && arr[idx - 1] == "q"
+			swap += letter
+		end
+		if VOWELS.include?(letter)
+			break
+		else
+			swap += letter
+		end
+	end
+	swap.length.times { arr.shift }
+	word = arr.join + "#{swap}ay"
+end
