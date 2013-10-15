2a10b39954a79aefd8517eb7451f40595169b5c8
Supernats <nathanseither@gmail.com>
2013-10-14 21:30:15 -0700

    Write simon says

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,41 @@
+def echo(message)
+	message
+end
+
+def shout(message)
+	message.upcase
+end
+
+def repeat(message, count = 2)
+	out = ""
+	i = 0
+	while i < count - 1
+		out += "#{message} "
+		i += 1
+	end
+	out += message
+	out
+end
+
+def start_of_word(message, count = 1)
+	message[0...(count)]
+end
+
+def first_word(message)
+	words = message.split
+	words[0]
+end
+
+def titleize(message)
+	words = message.split
+	words.each_with_index do |word, idx|
+		# Following code kinda sucks, come back later and:
+		# Add a database of "little words."" Refactor to do a check against it
+		# to DRY up the code and allow for more exception words.
+		if word == "and" || word == "the" || word == "over"
+			next unless idx == 0
+		end
+		word.capitalize!
+	end
+	words.join(" ")
+end
